8a2a84658bd0ddf525eaf0b70717e632cdfb2536
SarodipDiraka <oktavor@gmail.com>
2025-06-08 02:08:17 +0300

    Статичный билд обновлен снова

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,12 @@ const nextConfig = {
 
   trailingSlash: isExportMode,
 
-  ...(isExportMode && {
-    basePath: repoName,
-    assetPrefix: repoName,
-  }),
+  ...(isExportMode
+    ? {
+        basePath: repoName,
+        assetPrefix: repoName,
+      }
+    : {}),
 
   typescript: {
     ignoreBuildErrors: true
